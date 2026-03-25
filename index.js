94258c59f05c9a4f357002769c80e8d5d65ee33c
Shrest Kumar1 <shrestha251002@gmail.com>
2026-03-25 12:25:24 +0530

    Updating repo structure

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello, World!");
+import dotenv from "dotenv";
+import app from "./app.js";
+dotenv.config({
+    path: "./.env"
+});    
+
+const port=process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/default', (req, res) => {
+  res.send('Default page')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
+
+
